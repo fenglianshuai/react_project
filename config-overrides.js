73941f113d7d19d5d68cda122684a1491e7b40ad
fenglianshuai <1351716984@qq.com>
2020-03-17 17:30:10 +0800

    fix: customize-cra和react-app-rewired的定制化配置

---
+++ config-overrides.js
@@ -0,0 +1,10 @@
+/**
+ * @file config-overrides.js
+ * @author Leo Fenglianshuai
+ * 基于customize-cra和react-app-rewired的定制化配置文件
+ */
+
+ // 从customize-cra中引入一些相关的方法
+const { override } = require('customize-cra');
+
+module.exports = override();
