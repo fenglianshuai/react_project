98275068fcb548d81bd3499687858f2b04012200
fenglianshuai <1351716984@qq.com>
2020-03-17 17:20:24 +0800

    feat: 生成react项目

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { component, Component } from 'react'
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                app
+            </div>
+        )
+    }
+}
